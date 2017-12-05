ad0741a237e5a792a73299f8f8af42fa2bbb9e66
AKM Elias <akmelias11@gmail.com>
2017-12-05 18:14:08 +0600

    error solved

--- Anamoly_detection (1).py
+++ Anamoly_detection (1).py
@@ -3,16 +3,15 @@
 
 # In[12]:
 
-
 import math
-x=[3,4,4,5,5,1]
-y=[3,2,3,2,4,3]
+a=[3,4,4,5,5,1]
+b=[3,2,3,2,4,3]
 length=len(a)
 muX=sum(a)/length
 muY=sum(b)/length
 print("muX = ",muX,"muY=",muY)
-sigmaSquareX=float(sum([(x-c)**2 for x in a]))/length
-sigmaSquareY=float(sum([(x-d)**2 for x in b]))/length
+sigmaSquareX=float(sum([(x-muX)**2 for x in a]))/length
+sigmaSquareY=float(sum([(x-muY)**2 for x in b]))/length
 print("sigmaSquareX=",sigmaSquareX,"sigmaSquarey=",sigmaSquareY)
 
 sigmaX = math.sqrt(sigmaSquareX)
@@ -24,5 +23,6 @@ Nx = ((1/(math.sqrt(6.2832)*sigmaX)))*math.exp(-((x[-1]-muX)**2)/(2*sigmaSquareX
 Ny = ((1/(math.sqrt(6.2832)*sigmaY)))*math.exp(-((y[-1]-muY)**2)/(2*sigmaSquareY))
 print("Nx=",Nx,"Ny=",Ny)
 print(Nx*Ny)
+
 
 
